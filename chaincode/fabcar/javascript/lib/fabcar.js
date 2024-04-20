35cfc07b48b743d6b58785cb19cdbf4696fe46d7
Proma <wasima.annan@g.bracu.ac.bd>
2024-04-21 01:08:40 +0600

    all

---
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -0,0 +1,106 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class FabCar extends Contract {
+
+    async initLedger(ctx) {
+        console.info('============= START : Initialize Ledger ===========');
+          const cars = [
+        ];
+
+
+        for (let i = 0; i < cars.length; i++) {
+            cars[i].docType = 'user';
+            await ctx.stub.putState('USER' + i, Buffer.from(JSON.stringify(cars[i])));
+            console.info('Added <--> ', cars[i]);
+        }
+        console.info('============= END : Initialize Ledger ===========');
+    }
+
+    async queryCar(ctx, carNumber) {
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        console.log(carAsBytes.toString());
+        return carAsBytes.toString();
+    }
+  
+    
+
+    async createCar(ctx, carNumber, name, type, cof, cif, count, country, reputation) {
+        console.info('============= START : Create Car ===========');
+
+        const car = {
+            type,
+            docType: 'car',
+            name,
+            cof,
+            cif,
+            count,
+            country,
+            reputation
+        };
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : Create Car ===========');
+    }
+
+    async queryAllCars(ctx) {
+        const startKey = '';
+        const endKey = '';
+        const allResults = [];
+        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: key, Record: record });
+        }
+        console.info(allResults);
+        return JSON.stringify(allResults);
+    }
+
+    async changeCarOwner(ctx, carNumber, newName) {
+        console.info('============= START : changeCarOwner ===========');
+
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        const car = JSON.parse(carAsBytes.toString());
+        car.name = newName;
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : changeCarOwner ===========');
+    }
+
+
+    async updateReputation(ctx, carNumber, newrep) {
+        console.info('============= START : changeCarOwner ===========');
+
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        const car = JSON.parse(carAsBytes.toString());
+        car.reputation = newrep;
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : changeCarOwner ===========');
+    }
+
+}
+
+module.exports = FabCar;
---
+++ fabcar/javascript/updateReputation.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+const fs = require('fs');
+
+
+async function main( params ) {
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
+        const nid = params.nid
+        const key = params.key
+        const reputation=params.reputation
+
+
+        const users = [
+            { NID: '1', role: 'admin' },
+            { NID: '2', role: 'admin' },
+            { NID: '3', role: 'admin' },
+            // other users
+          ];
+
+          function isAdmin(userNID) {
+            const user = users.find(u => u.NID === userNID);
+            return user && user.role === 'admin';
+          }
+
+          if (!isAdmin(nid)) {
+            throw new Error('Only admins can update the company reputation');
+        }
+          
+
+  
+        // const type = params.type
+        // const count = params.count
+        // const country = params.country
+
+
+        // Submit the specified transaction.
+        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
+        await contract.submitTransaction('updateReputation', `${ key }`, `${ reputation}`)
+        console.log('Change Owner Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } 
+     catch (error) {
+        console.error(`Failed to change owner transaction: ${error.message}`);
+        return Promise.reject(error);
+    }
+}
+
+// main();
+module.exports = { main }
